8537e60a3a354c8e53e57655f24cf16489cc4706
Polawich Duangsuk <42580909+polawich@users.noreply.github.com>
2023-12-22 10:36:03 +0700

    Update speedtest-lab.sh

--- speedtest-lab.sh
+++ speedtest-lab.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 header=$(speedtest-cli --secure --csv-header)
-echo $header > speedtest-chotipat.csv
+echo $header > speedtest-polawich.csv
 servers=$(speedtest-cli --list | awk -F ')' 'NR > 1 {print $1}')
 for server in $servers
 do
 echo $(date) "Server ID: $server"
-speedtest-cli --secure --server $server --csv >> speedtest-chotipat.csv
+speedtest-cli --secure --server $server --csv >> speedtest-polawich.csv
 echo "Done"
 done
 $
